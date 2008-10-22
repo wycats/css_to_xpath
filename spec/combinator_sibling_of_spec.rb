423769208530404b4cadccdaba4d4c0210bae88f
Carl Lerche <carllerche@mac.com>
2008-10-22 00:33:57 -0700

    Finished specs for the general sibling combinator

--- spec/combinator_sibling_of_spec.rb
+++ spec/combinator_sibling_of_spec.rb
@@ -1,9 +1,33 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "The simbling of combinator" do
+describe "The sibling of combinator" do
   
-  it "should be awesome" do
-    
+  it "<div ~ p> should return the p tag" do
+    HTML.should have_nodes("div ~ p", 3)
+  end
+  
+  it "<div ~ p[class]> should only have 2 p tags" do
+    HTML.should have_nodes("div ~ p[class]", 2)
+  end
+  
+  it "<p[class] ~ p[class] should only match one" do
+    HTML.should have_nodes("p[class] ~ p[class]", 1)
+  end
+  
+  it "<p[class] ~ p should have two" do
+    HTML.should have_nodes("p[class] ~ p", 2)
+  end
+  
+  it "<div ~ div> should only have one" do
+    HTML.should have_nodes("div ~ div", 1)
+  end
+  
+  it "<div ~ span> should have none" do
+    HTML.should have_nodes("div ~ span", 0)
+  end
+  
+  it "<html ~ body> should have none" do
+    HTML.should have_nodes("html ~ body", 0)
   end
   
 end
